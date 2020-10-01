567e83fba7814d7876f666b9c43923c15350311b
RyanPlanteNJ <ryanm.plante@gmail.com>
2020-09-30 21:43:00 -0400

    fixed issue with search

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,24 +1,24 @@
 # TO-DO: Complete the selection_sort() function below
 def selection_sort(arr):
     # loop through n-1 elements
-    for i in range(0, len(arr) - 1):
-        cur_index = i
-        smallest_index = cur_index
-        # TO-DO: find next smallest element
-        # (hint, can do in 3 loc)
-        # Your code here
-
-
-        # TO-DO: swap
-        # Your code here
-
+    current = 0
+    while current < len(arr):
+        smallest = current
+        for i in range(current, len(arr)):
+            if(arr[smallest] > arr[i]):
+                smallest = i
+        arr.insert(current, arr.pop(smallest))
+        current += 1
     return arr
 
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort(arr):
     # Your code here
-
+    for i in range(0, len(arr)):
+        for j in range(0, len(arr)):
+            if arr[i] < arr[j]:
+                arr[i], arr[j] = arr[j], arr[i]
 
     return arr
 
@@ -31,11 +31,11 @@ data. The idea behind this algorithm then is that we can create "buckets"
 from 0 up to the max value. This is most easily done by initializing an
 array of 0s whose length is the max value + 1 (why do we need this "+ 1"?).
 
-Each buckets[i] then is responsible for keeping track of how many times 
+Each buckets[i] then is responsible for keeping track of how many times
 we've seen `i` in the input set of data as we iterate through it.
 Once we know exactly how many times each piece of data in the input set
-showed up, we can construct a sorted set of the input data from the 
-buckets. 
+showed up, we can construct a sorted set of the input data from the
+buckets.
 
 What is the time and space complexity of the counting sort algorithm?
 '''
--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,6 +1,8 @@
 def linear_search(arr, target):
     # Your code here
-
+    for i in range(0, len(arr)):
+        if arr[i] == target:
+            return i
 
     return -1   # not found
 
@@ -9,6 +11,18 @@ def linear_search(arr, target):
 def binary_search(arr, target):
 
     # Your code here
+    start = 0
+    end = (len(arr)-1)
 
-
+    found = False
+    while end >= start and not found:
+        middle_int = (start + end) // 2
+        if arr[middle_int] == target:
+            found = True
+            return middle_int
+        else:
+            if target < arr[middle_int]:
+                end = middle_int - 1
+            if target > arr[middle_int]:
+                start = middle_int + 1
     return -1  # not found
